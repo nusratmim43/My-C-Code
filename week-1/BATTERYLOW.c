b36bd9fdca3973f0460766aaf94edd73db661445
Nusrat Jahan Mim <114955198+nusratmim43@users.noreply.github.com>
2023-02-23 22:51:06 +0600

    Add files via upload

---
+++ week-1/BATTERYLOW.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T,i;
+	scanf("%d",&T);
+	for(i=1;i<=T;i++)
+	{
+	if(T<=15){
+	printf("YES\n");}
+	else 
+	printf("NO\n");
+	}
+	return 0;
+}
+
